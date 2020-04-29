405022b8274b46d796d1414671136923dde25f2d
AbsenceSliet <1584123890@qq.com>
2020-04-29 16:53:41 +0800

    :sparkles:  search history

--- src/pages/Home/hot.js
+++ src/pages/Home/hot.js
@@ -1,7 +1,11 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { hotSearch } from "@/constants/api";
 import { useSelector, useDispatch } from "react-redux";
-import { setSearchHot,setSearchValue, setSearchHistory } from "@/store/modules/search/action";
+import {
+  setSearchHot,
+  setSearchValue,
+  setSearchHistory,
+} from "@/store/modules/search/action";
 import { isEmpty, map, lte } from "lodash";
 import CustomIcon from "@/components/CustomIcon";
 import { useHistory } from "react-router-dom";
@@ -12,51 +16,63 @@ const SearchHistory = () => {
   const dispatch = useDispatch();
   const historys = useSelector((state) => state.search.historys);
   const router = useHistory();
+  const [historyData, setHistoryData] = useState(historys);
+
   const clearHistory = () => {
-    dispatch(setSearchHistory(''));
+    dispatch(setSearchHistory(""));
+    setHistoryData([]);
+  };
+  const historyLink = (item) => {
+    dispatch(setSearchHistory(item));
+    dispatch(setSearchValue(item));
+    router.push(`/search?query=${item}`);
   };
   useEffect(() => {
-    console.log(historys,'0000000historys')
+    console.log(historys, "searchhistory");
   }, [historys]);
-  return (
-    !isEmpty(historys) ? <div className="historys">
-    <p className="historys_header">
-      <span>搜索历史</span>
-      <CustomIcon
-        className="icon-delete"
-        type="delete"
-        onClick={clearHistory}
-      />
-    </p>
-    <ul>
-      {map(historys, (item, index) => {
-        return (
-          <li
-            key={index}
-            onClick={() => {
-              router.push(`/search?query=${item}`);
-            }}
-          >
-            {item}
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-  :''
+  return historyData.length > 0 ? (
+    <div className="historys">
+      <p className="historys_header">
+        <span>搜索历史</span>
+        <CustomIcon
+          className="icon-delete"
+          type="delete"
+          onClick={clearHistory}
+        />
+      </p>
+      <ul>
+        {map(historys, (item, index) => {
+          return (
+            <li
+              key={index}
+              onClick={() => {
+                historyLink(item);
+              }}
+            >
+              {item}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    ""
   );
 };
 const HotItem = ({ item, number }) => {
   const { searchWord, iconUrl, content } = item;
   const dispatch = useDispatch();
   const router = useHistory();
-  const hotClickHandle =value=>{
+  const hotClickHandle = (value) => {
     dispatch(setSearchHistory(value));
     dispatch(setSearchValue(value));
     router.push(`/search?query=${value}`);
-  }
+  };
   return (
-    <li className={classNames("item", lte(number, 4) ? "active" : "")} onClick={()=>hotClickHandle(searchWord)}>
+    <li
+      className={classNames("item", lte(number, 4) ? "active" : "")}
+      onClick={() => hotClickHandle(searchWord)}
+    >
       <p className="item_sort">{number}</p>
       <div className="item_content">
         <h6>
@@ -71,7 +87,7 @@ const HotItem = ({ item, number }) => {
 export default () => {
   const dispatch = useDispatch();
   const hotData = useSelector((state) => state.search.hot);
-
+  console.log("hot - page - show");
   useEffect(() => {
     if (!hostLoaded) {
       hostLoaded = true;
