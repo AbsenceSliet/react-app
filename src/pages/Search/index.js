56fa3dab42a5c22fb336c19a40a3a36d65171eb1
AbsenceSliet <1584123890@qq.com>
2020-04-30 15:43:08 +0800

    :beers:  add search

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { getSearchMusic } from "@/constants/api";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
@@ -6,26 +6,34 @@ import { getUrlQuery } from "@/utils/tools";
 import BScroll from "@better-scroll/core";
 import Pullup from "@better-scroll/pull-up";
 import "./index.scss";
+import highlight from "@/utils/highlight";
 BScroll.use(Pullup);
 const limit = 20;
+const SearchItem = (props) => {};
 export default () => {
   const route = useHistory();
+  const [searchList, setSearchList] = useState([]);
   const { query } = getUrlQuery();
+  const bs = useRef(null);
   useEffect(() => {
-    new BScroll("#search__list", {
+    bs.current = new BScroll("#search__list", {
       scrollY: true,
       pullUpLoad: true,
+      probeType: 1,
     });
     getSearchMusic({ keywords: query, offset: 0, limit }).then((res) => {
-      console.log(res);
+      console.log(res.data.result);
+      setSearchList(res.data.result.songs);
+      bs.current.refresh();
     });
-  }, []);
+  }, [query]);
   return (
     <div className="search_main" id="search__list">
       <ul>
-        {new Array(20).fill(0).map((item, index) => (
-          <li key={index}>{index}</li>
-        ))}
+        {searchList &&
+          searchList.map((item, index) => (
+            <li key={index}>{highlight(item.name, query)}</li>
+          ))}
       </ul>
     </div>
   );
---
+++ src/utils/highlight.js
@@ -0,0 +1,20 @@
+import { createElement, Fragment } from "react";
+
+const hightlight = (
+  contents,
+  keywords,
+  tag = "span",
+  props = { className: "hightlight" }
+) => {
+  let regex = new RegExp(`(${keywords})`, "gi");
+  const nodes = contents.split(regex).map((res, index) => {
+    console.log(res, "00000");
+    if (res === keywords) {
+      return createElement(tag, { ...props, key: index }, res);
+    }
+    return res;
+  });
+
+  return createElement(Fragment, null, nodes);
+};
+export default hightlight;
